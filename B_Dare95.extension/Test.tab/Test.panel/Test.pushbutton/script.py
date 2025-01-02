87353d9168530b8422407a60cfddeece6f426122
B-Dare-95 <archmohamed95@gmail.com>
2025-01-02 17:02:27 +0200

    2/1/2025

--- B_Dare95.extension/Test.tab/Test.panel/Test.pushbutton/script.py
+++ B_Dare95.extension/Test.tab/Test.panel/Test.pushbutton/script.py
@@ -14,42 +14,64 @@ doc         = __revit__.ActiveUIDocument.Document
 selection   = uidoc.Selection
 app         = __revit__.Application
 active_view = doc.ActiveView
-output = script.get_output()
+output      = script.get_output()
 
-# try: element_reference = selection.PickObject(ObjectType.Element,"Select A Floor")
-# except: forms.alert("Script is canceled.",exitscript=True,title="Script Canceled.")
-#
-# floor_element = doc.GetElement(floor_reference.ElementId)
-#
-# source_floor_height = floor_element.get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).AsDouble()
+########################################################################################################################
 
-# with forms.WarningBar(title="Pick Floor to match Height:", handle_esc=True):
-#     while True:
-#
-#         try: element_reference = selection.PickObject(ObjectType.Element,"Select an element")
-#
-#         except: break
-#
-#         element = doc.GetElement(element_reference.ElementId)
-#         element_category_id = element.Category.Id
-#         element_name = element.Name
-#
-#         with Transaction(doc,"Get all Similar Elements") as t:
-#
-#                 t.Start()
-#
-#                 collector = FilteredElementCollector(doc) \
-#                     .OfCategoryId(element_category_id) \
-#                     .WhereElementIsNotElementType() \
-#                     .ToElements()
-#
-#                 el_ids = [el.Id for el in collector if el.Name == element_name]
-#
-#                 List_el_ids = List[ElementId](el_ids)
-#
-#                 uidoc.Selection.SetElementIds(List_el_ids)
-#
-#                 t.Commit()
+all_rooms = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms).WhereElementIsNotElementType().ToElements()
+
+room_dictionary = {}
+
+for room in all_rooms:
+
+#Get Room Location Point
+    room_location = room.Location
+    room_location_point = room_location.Point
+
+#Get Room Boundaries
+    room_bounds_list = room.GetBoundarySegments(SpatialElementBoundaryOptions())
+
+    for bound_list in room_bounds_list:
+        room_dictionary.update({room_location_point:bound_list})
+
+
+
+for k,v in room_dictionary.items():
+    #Get Lines from Boundary Segment Lists
+    for bound in v:
+        #Get The Bounding Element
+        bounding_element = doc.GetElement(bound.ElementId)
+
+        #Get The Midpoint of Bounding Element
+        if isinstance(bounding_element,Wall):
+            location_curve = bounding_element.Location
+            bound_midpoint = location_curve.Curve.Evaluate(0.5,True)
+
+            #Create Vector from Boundary Midpoint to Room Location Point
+            vektor =  bound_midpoint.Add(k)
+            #Negate The Vector
+            opposite_vektor = vektor.Negate()
+
+            inspection_point = bound_midpoint.Move(opposite_vektor)
+            print(inspection_point)
+
+            moved_curve = location_curve.Move(opposite_vektor)
+
+            new_inspection_point = moved_curve.Curve.Evaluate(0.5,True)
+
+            print(new_inspection_point)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
